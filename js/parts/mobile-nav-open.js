5f7418f1dd362572b1aa08becbe3820ad7512b7b
Yuriy Savchenko <yurkaronin@gmail.com>
2024-10-24 22:43:23 +0300

    якорное мобильное меню и доработка стилей модального окна

--- js/parts/mobile-nav-open.js
+++ js/parts/mobile-nav-open.js
@@ -5,4 +5,30 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.classList.toggle('mobile-nav-open');
   });
 
+  // Обработка кликов по якорным ссылкам
+  var anchorLinks = document.querySelectorAll('.mobile-nav__link[data-anchor="true"]');
+  if (anchorLinks.length > 0) {
+    anchorLinks.forEach(function (anchorLink) {
+      anchorLink.addEventListener('click', function (event) {
+        event.preventDefault();  // Предотвращаем стандартный переход по ссылке
+        var targetId = this.getAttribute('href');  // Получаем ID цели
+        var targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+          // Плавная прокрутка до цели с учетом высоты фиксированного хедера
+          const headerHeight = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--header-height'));
+          const rect = targetElement.getBoundingClientRect();
+          const offset = Math.floor(rect.top + window.pageYOffset - headerHeight);
+
+          window.scrollTo({
+            top: offset,
+            behavior: 'smooth'
+          });
+        }
+
+        // Закрываем мобильное меню
+        document.body.classList.remove('mobile-nav-open');
+      });
+    });
+  }
 });
