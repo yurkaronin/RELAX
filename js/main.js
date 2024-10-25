292e2b511882fc2b6faf77e785f24d5f1a6a6921
RoninsWork <47382042+yurkaronin@users.noreply.github.com>
2024-10-25 16:13:45 +0300

    Доработка слайдеров с отзывами

--- js/main.js
+++ js/main.js
@@ -31,36 +31,84 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
   // Слайдеры в отзывах
-  // большие фото
+  // Инициализация большого слайдера
   if (document.querySelector(".js-reviews-slider-big")) {
     var reviewsSliderBig = new Swiper(".js-reviews-slider-big .swiper", {
       loop: true,
       spaceBetween: 16,
-      effect: "fade",
-    });
-  }
-  if (document.querySelector(".js-reviews-slider-text")) {
-    var reviewsSliderText = new Swiper(".js-reviews-slider-text .swiper", {
-      loop: true,
-      spaceBetween: 16,
-      // effect: "fade",
+      autoplay: {
+        delay: 4000, // Автоматическая смена слайда каждые 4 секунды
+        disableOnInteraction: false // Автоплей не останавливается при взаимодействии с пользователем
+    },
     });
   }
 
+  // Инициализация миниатюр
   if (document.querySelector(".js-reviews-slider-min")) {
     var reviewsSliderMin = new Swiper(".js-reviews-slider-min .swiper", {
       loop: true,
       spaceBetween: 16,
       slidesPerView: 3,
+      breakpoints: {
+        768: {
+          spaceBetween: 16,
+          slidesPerView: 3,
+        },
+        320: {
+          slidesPerView: 'auto',
+          spaceBetween: 10,
+        }
+      },
     });
   }
 
+  // Связываем миниатюры с основным слайдером
+  reviewsSliderMin.slides.forEach((slide, index) => {
+    slide.addEventListener('click', () => {
+      reviewsSliderBig.slideToLoop(index); // Переключаем большой слайдер на нужный индекс
+    });
+  });
+
+  // Синхронизация миниатюр при смене слайда в большом слайдере
+  reviewsSliderBig.on('slideChange', () => {
+    reviewsSliderMin.slideToLoop(reviewsSliderBig.realIndex); // Переключаем миниатюры на текущий слайд большого слайдера
+  });
 
 
+  //   // большие фото
+  //   if (document.querySelector(".js-reviews-slider-big")) {
+  //     var reviewsSliderBig = new Swiper(".js-reviews-slider-big .swiper", {
+  //       loop: true,
+  //       spaceBetween: 16,
+  //       // effect: "fade",
+  //     });
+  //   }
 
-});
+  //   // Миниатюры
+  //   if (document.querySelector(".js-reviews-slider-min")) {
+  //     var reviewsSliderMin = new Swiper(".js-reviews-slider-min .swiper", {
+  //       loop: true,
+  //       spaceBetween: 16,
+  //       slidesPerView: 3,
+  //       breakpoints: {
+  //         // Когда ширина окна >= 768px
+  //         768: {
+  //           spaceBetween: 16,
+  //           slidesPerView: 3,
+  //         },
+  //         // Когда ширина окна < 768px
+  //         320: {
+  //           slidesPerView: 'auto',
+  //           spaceBetween: 10,
+  //         }
+  //       }
+
+  //     });
+  //   }
+
+  });
 
 
-// техническая часть - УДАЛИТЬ НА ПРОДАКШЕНЕ!
-// получить в консоли элемент, по которому кликнули
-document.addEventListener("click", (e) => console.log(e.target));
+  // техническая часть - УДАЛИТЬ НА ПРОДАКШЕНЕ!
+  // получить в консоли элемент, по которому кликнули
+  document.addEventListener("click", (e) => console.log(e.target));
